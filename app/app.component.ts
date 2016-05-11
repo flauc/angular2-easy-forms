98141a2543ef0e5f84352308e1522cb048308aa1
flauc <filip.lauc93@gmail.com>
2016-05-11 21:09:49 +0200

    feat: Setup of folder structure

---
+++ app/app.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'app',
+    template: ''
+})
+export class AppComponent {
+    
+}
---
+++ system.config.js
@@ -0,0 +1,39 @@
+(function(global) {
+    var map = {
+        'app': 'app',
+        'rxjs': 'node_modules/rxjs',
+        'angular2-in-memory-web-api': 'node_modules/angular2-in-memory-web-api',
+        '@angular': 'node_modules/@angular'
+    },
+        packages = {
+            'app': { main: 'main.js',  defaultExtension: 'js' },
+            'rxjs': { defaultExtension: 'js' },
+            'angular2-in-memory-web-api': { defaultExtension: 'js' }
+        },
+        packageNames = [
+            '@angular/common',
+            '@angular/compiler',
+            '@angular/core',
+            '@angular/http',
+            '@angular/platform-browser',
+            '@angular/platform-browser-dynamic',
+            '@angular/router',
+            '@angular/router-deprecated',
+            '@angular/testing',
+            '@angular/upgrade'
+        ];
+
+    packageNames.forEach(function(pkgName) {
+        packages[pkgName] = { main: 'index.js', defaultExtension: 'js' };
+    });
+
+    var config = {
+        map: map,
+        packages: packages
+    };
+
+    if (global.filterSystemConfig) { global.filterSystemConfig(config); }
+
+    System.config(config);
+
+})(this);
