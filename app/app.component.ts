9f1fbd2238fd1247b715a547fbe9f447ba024718
flauc <filip.lauc93@gmail.com>
2016-05-18 18:17:49 +0200

    feat: working on page

---
+++ app/app.component.ts
@@ -0,0 +1,94 @@
+import {Component} from '@angular/core';
+import {EasyFormsComponent} from 'easy-forms';
+
+function startsWithNumber(control) {
+
+    if (control.value !== '' && !isNaN(control.value.charAt(0))) {
+        return {'startsWithNumber': true};
+    }
+
+    return null;
+}
+
+@Component({
+    selector: 'app',
+    directives: [EasyFormsComponent],
+    template: `
+        <h1>Testing</h1>
+        <easy-forms [data]="data" (onSubmit)="onSubmit($event)" (onChanges)="onChanges($event)"></easy-forms>
+    `
+})
+export class AppComponent {
+    constructor() {}
+
+    public data = {
+        settings: {
+            submitButtonText: 'Send',
+        },
+        questions: [
+            {
+                type: 'text',
+                key: 'firstName',
+                value: 'John Doe',
+                label: 'First Name',
+                validation: [
+                    {type: 'required'},
+                    {type: 'minLength', value: 5, message: 'Please enter a name longer then 5 characters'},
+                    {type: 'pattern', value: '^[a-zA-Z ]+$', message: 'Only letters and spaces are allowed'}
+                ]
+            },
+            {
+                type: 'password',
+                key: 'password',
+                label: 'Password',
+                validation: [
+                    {type: 'required'},
+                    {type: 'custom', value: startsWithNumber, message: 'Please dont start with a number'}
+                ]
+            },
+            {
+                type: 'dropdown',
+                key: 'address',
+                label: 'Address',
+                value: 'osijek',
+                order: 2,
+                options: [
+                    {value: 'osijek', name: 'Osijek'},
+                    {value: 'zagreb', name: 'Zagreb'}
+                ]
+            },
+            {
+                type: 'radio',
+                key: 'gender',
+                label: 'Gender',
+                value: 'male',
+                options: [
+                    {value: 'male', name: 'Male'},
+                    {value: 'female', name: 'Female'}
+                ]
+            },
+            {
+                type: 'checkbox',
+                key: 'things',
+                label: 'Things You Like',
+                values: ['pokemon', 'starWars'],
+                options: [
+                    {value: 'starWars', name: 'Star Wars'},
+                    {value: 'batlefield', name: 'Batlefield'},
+                    {value: 'pokemon', name: 'Pokemon'}
+                ],
+                validation: [
+                    {type: 'required'}
+                ]
+            }
+        ]
+    };
+
+    onSubmit(event) {
+        console.log(event)
+    }
+
+    onChanges(event) {
+        console.log(event)
+    }
+}
---
+++ system.config.js
@@ -0,0 +1,41 @@
+(function(global) {
+    var map = {
+            'app': 'app',
+            'rxjs': 'node_modules/rxjs',
+            'angular2-in-memory-web-api': 'node_modules/angular2-in-memory-web-api',
+            '@angular': 'node_modules/@angular',
+            'easy-forms': 'node_modules/angular2-easy-forms'
+        },
+        packages = {
+            'app': { main: 'main.js', defaultExtension: 'js' },
+            'rxjs': { defaultExtension: 'js' },
+            'angular2-in-memory-web-api': { defaultExtension: 'js' },
+            'easy-forms': {main: 'component.js', defaultExtension: 'js'}
+        },
+        packageNames = [
+            '@angular/common',
+            '@angular/compiler',
+            '@angular/core',
+            '@angular/http',
+            '@angular/platform-browser',
+            '@angular/platform-browser-dynamic',
+            '@angular/router',
+            '@angular/router-deprecated',
+            '@angular/testing',
+            '@angular/upgrade'
+        ];
+
+    packageNames.forEach(function(pkgName) {
+        packages[pkgName] = { main: 'index.js', defaultExtension: 'js' };
+    });
+
+    var config = {
+        map: map,
+        packages: packages
+    };
+
+    if (global.filterSystemConfig) { global.filterSystemConfig(config); }
+
+    System.config(config);
+
+})(this);
