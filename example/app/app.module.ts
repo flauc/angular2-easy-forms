5d8b65a1140b13a59cb39f7fb82b1736f579fd7a
flauc <filip.lauc93@gmail.com>
2016-09-02 09:37:34 +0200

    fix: Moved to CommonModule

--- example/app/app.module.ts
+++ example/app/app.module.ts
@@ -1,13 +1,11 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
-import {FormsModule} from '@angular/forms';
 import {AppComponent} from './app.component';
 import {EasyFormsModule} from 'angular2-easy-forms';
 
 @NgModule({
     imports: [
         BrowserModule,
-        FormsModule,
         EasyFormsModule
     ],
     providers: [],
--- src/easy-forms.module.ts
+++ src/easy-forms.module.ts
@@ -1,12 +1,12 @@
 import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {CommonModule} from '@angular/common';
+import {ReactiveFormsModule} from '@angular/forms';
 import {EasyFormsComponent} from './easy-forms.component';
 import {QuestionComponent} from '../lib/question.component';
 import {ControlGroupService} from './control-group.service';
 
 @NgModule({
-    imports: [BrowserModule, ReactiveFormsModule],
+    imports: [CommonModule, ReactiveFormsModule],
     declarations: [EasyFormsComponent, QuestionComponent],
     providers: [ControlGroupService],
     exports: [EasyFormsComponent]
